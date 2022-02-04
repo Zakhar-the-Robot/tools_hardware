80a809cb4b7c842d1b0587b86a09932d288085ab
Andrei Gramakov <mail@agramakov.me>
2022-02-05 00:36:36 +0100

    wip 11

--- can_tool/main_node/src/main.cpp
+++ can_tool/main_node/src/main.cpp
@@ -1,31 +1,17 @@
 #include <Arduino.h>
 #include <SPI.h>
-#include <LibPrintf.h>
 #include "zakhar_canbus.h"
 #include "CanShield.hpp"
 #include "CanBus.hpp"
-#include <MemoryFree.h>
-
-#define LOG_FREEMEM(x) (printf("%s (free mem: %d)\n", x, freeMemory()))
-bool res = false;
-bool testing = true;
-
 
 void setup()
 {
-
-    while (!Serial)
-        ;
     Serial.begin(9600);
-    printf("Example: CAN Test Node\n");
     canShield.Start();
     canShield.SetGreen0(true);
     devCanBus.Start(10, 1, 1);
-    // devCanBus.Stop();
 }
 
 void loop()
 {
-    // printf("Example: CAN Test Nod\n");
-
 }
