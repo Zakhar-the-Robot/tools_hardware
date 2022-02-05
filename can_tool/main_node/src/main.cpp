547cdbf5818b47b9aede1408f552f1b12d4155bd
Andrei Gramakov <mail@agramakov.me>
2022-02-05 14:59:25 +0100

    wip 12

--- can_tool/main_node/src/main.cpp
+++ can_tool/main_node/src/main.cpp
@@ -3,13 +3,29 @@
 #include "zakhar_canbus.h"
 #include "CanShield.hpp"
 #include "CanBus.hpp"
+#include <LibPrintf.h>
+
+void RxCallback(const CanBus *dev, const can_frame &msg)
+{
+    printf("0x%08lx : %02x : %02x : %02x : %02x : %02x : %02x : %02x : %02x\n",
+           msg.can_id,
+           msg.data[7],
+           msg.data[6],
+           msg.data[5],
+           msg.data[4],
+           msg.data[3],
+           msg.data[2],
+           msg.data[1],
+           msg.data[0]);
+}
 
 void setup()
 {
-    Serial.begin(9600);
+    Serial.begin(115200);
     canShield.Start();
     canShield.SetGreen0(true);
     devCanBus.Start(10, 1, 1);
+    devCanBus.SetRxCallback(RxCallback);
 }
 
 void loop()
