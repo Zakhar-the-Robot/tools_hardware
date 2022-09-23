484f076444b031176fa0f3bba9d3e8fef21082be
Andrei Gramakov <mail@agramakov.me>
2022-09-24 00:36:12 +0200

    Sync frame and better math

--- audio_stereo_recorder/src/main.cpp
+++ audio_stereo_recorder/src/main.cpp
@@ -1,17 +1,19 @@
 #include <Arduino.h>
 
-#define LEFT_MIC_ONLY 1
 #define MAX_ADC_VAL 255U
 
-const uint32_t SAMPLE_RATE_HZ = 1000;
-const uint64_t SERIAL_BAUDRATE = 460800;
+const uint32_t SAMPLE_RATE_HZ = 10;
+const uint64_t SERIAL_BAUDRATE = 400000;
+const uint32_t SYNC_FRAMES = 32;
+
 const uint8_t PIN_MIC_LEFT = A0;
 const uint8_t PIN_MIC_RIGHT = A1;
 
+uint8_t frame_count = 0;
 uint8_t left = 128;
 uint8_t right = 0;
 uint64_t last_transmission_time = 0;
-const uint32_t PERIOD_US = 10 ^ 6 / SAMPLE_RATE_HZ;
+const uint32_t PERIOD_US = 1000000 / SAMPLE_RATE_HZ;
 
 void setup()
 {
@@ -28,32 +30,37 @@ void setup()
     }
 }
 
-void _read_mics(uint8_t &left_mic, uint8_t &right_mic)
+void read_data(uint8_t &left_mic, uint8_t &right_mic)
 {
     left_mic = MAX_ADC_VAL - analogRead(PIN_MIC_LEFT);
     right_mic = MAX_ADC_VAL - analogRead(PIN_MIC_RIGHT);
 }
 
-void _send_data(uint8_t left_mic, uint8_t right_mic)
+void send_frame(uint8_t left_mic, uint8_t right_mic)
 {
-    Serial.println(left_mic);
-    // Serial.write(left_mic);
-#ifndef LEFT_MIC_ONLY
+    Serial.write(left_mic);
     Serial.write(right_mic);
-#endif
+    last_transmission_time = micros();
 }
 
-void read_n_send()
-{
-    _read_mics(left, right);
-    _send_data(left, right);
-    last_transmission_time = micros();
+void handle_sync(){
+    
+    if (frame_count >= SYNC_FRAMES){
+        frame_count = 0;
+        Serial.write(0x55);
+        Serial.write(0x55);
+        Serial.write(0x55);
+        Serial.write(0x55);
+    }
+    frame_count++;
 }
 
 void loop()
 {
     if (micros() - last_transmission_time >= PERIOD_US)
     {
-        read_n_send();
+        handle_sync();
+        read_data(left, right);
+        send_frame(left, right);
     }
 }
